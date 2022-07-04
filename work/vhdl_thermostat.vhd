6519cf5c3f4144536dcf6a752f2a4fb51d889ad3
Saviour <owolabisaviour@gmail.com>
2022-07-04 14:23:29 +0100

    Lab 02: Add logic for controlling furnace and AC; Add Testbench

--- work/vhdl_thermostat.vhd
+++ work/vhdl_thermostat.vhd
@@ -1,17 +1,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity THERMOSTATDISPLAY is
+entity THERMOSTAT is
     port (
         CURRENT_TEMP: in std_logic_vector(6 downto 0);
         DESIRED_TEMP: in std_logic_vector(6 downto 0);
         DISPLAY_SELECT: in bit;
-        TEMP_DISPLAY: out std_logic_vector(6 downto 0)
+        COOL: in bit;
+        HEAT: in bit; 
+        TEMP_DISPLAY: out std_logic_vector(6 downto 0);
+        AC_ON: out bit; 
+        FURNACE_ON: out bit
     );
-end THERMOSTATDISPLAY;
+end THERMOSTAT;
 
 
-architecture BEHAVIOUR of THERMOSTATDISPLAY is
+architecture BEHAVIOUR of THERMOSTAT is
 
 begin
     process(CURRENT_TEMP, DESIRED_TEMP, DISPLAY_SELECT)
@@ -21,5 +25,13 @@ begin
         else
             TEMP_DISPLAY <= DESIRED_TEMP;
         end if; 
-    end process; 
+    end process;
+
+
+    AC_ON <= '1' WHEN COOL = '1' AND DESIRED_TEMP < CURRENT_TEMP ELSE '0';
+
+    WITH (HEAT = '1' AND (DESIRED_TEMP < CURRENT_TEMP)) SELECT 
+        FURNACE_ON <= '1' WHEN TRUE,
+        '0' WHEN OTHERS;
+
 end BEHAVIOUR;

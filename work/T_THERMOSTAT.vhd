d36d18717bcfd8279d1af8ae9daac236d60535a3
Saviour <owolabisaviour@gmail.com>
2022-07-08 17:36:37 +0100

    Refactor logic into state machine; modify testbench to test new logic

--- work/T_THERMOSTAT.vhd
+++ work/T_THERMOSTAT.vhd
@@ -9,8 +9,22 @@ end T_THERMOSTAT;
 
 architecture TEST of T_THERMOSTAT is 
 
-    signal CURRENT_TEMP, DESIRED_TEMP, TEMP_DISPLAY: std_logic_vector(6 downto 0);
-    signal COOL, HEAT, AC_ON, FURNACE_ON, DISPLAY_SELECT, CLK: bit;
+    signal CLK: std_logic := '0';
+    
+    signal CURRENT_TEMP: std_logic_vector(6 downto 0);  
+    signal DESIRED_TEMP: std_logic_vector(6 downto 0);
+    
+    signal COOL: std_logic;
+    signal HEAT: std_logic;
+    
+    signal DISPLAY_SELECT: std_logic;
+    
+    signal FURNACE_HOT: std_logic;
+    signal AC_READY: std_logic; 
+
+    signal TEMP_DISPLAY: std_logic_vector(6 downto 0);
+    signal AC_ON: std_logic;
+    signal FURNACE_ON: std_logic;
 
     procedure waitForClockToFall is
     begin
@@ -25,24 +39,26 @@ architecture TEST of T_THERMOSTAT is
     component THERMOSTAT is
         port (
 
-            CLK: in bit; 
-
+            CLK: in std_logic :='0'; 
 
             CURRENT_TEMP: in std_logic_vector(6 downto 0);
             DESIRED_TEMP: in std_logic_vector(6 downto 0);
-            DISPLAY_SELECT: in bit;
-            COOL: in bit;
-            HEAT: in bit; 
+            DISPLAY_SELECT: in std_logic;
+            COOL: in std_logic;
+            HEAT: in std_logic; 
             TEMP_DISPLAY: out std_logic_vector(6 downto 0);
-            AC_ON: out bit; 
-            FURNACE_ON: out bit
+
+            AC_ON: out std_logic; 
+            AC_READY: in std_logic; 
+
+            FURNACE_ON: out std_logic;
+            FURNACE_HOT: in std_logic;
+
+            FAN_ON: out std_logic
         );
     end component;
 
-
-
 begin
-
     UUT: THERMOSTAT port map(CURRENT_TEMP => CURRENT_TEMP,
         DESIRED_TEMP => DESIRED_TEMP,
         DISPLAY_SELECT => DISPLAY_SELECT,
@@ -50,20 +66,20 @@ begin
         HEAT => HEAT,
         TEMP_DISPLAY => TEMP_DISPLAY,
         AC_ON => AC_ON,
+        AC_READY => AC_READY,
         FURNACE_ON => FURNACE_ON,
+        FURNACE_HOT => FURNACE_HOT,
         CLK => CLK);
-
-
-
 
 clock_process:
 process
     variable t : time := 0 ps;
 begin
     loop
-        t := t + 10 ps; 
-        wait for 10 ps;
+        t := t + 100 ps; 
+        wait for 100 ps;
         CLK <= not CLK;
+	exit when t >= 10000 ps;
     
     end loop; 
     wait;    
@@ -71,7 +87,13 @@ end process;
 
 main_process:
 process
+    --variable count: integer := 0; 
 begin
+    -- TEST : loop
+    --     exit when count >= 3; 
+    -- end loop; -- TEST
+
+    --count := count + 1;
     waitForClockToFall;
     CURRENT_TEMP <= "1111111";
     DESIRED_TEMP <= "0000000";
@@ -79,50 +101,57 @@ begin
     HEAT <= '0';
     COOL <= '0';
 
-    wait for 10 ps;
+    --wait for 10 ps;
+    -- STATE: IDLE
     waitForClockToFall;
-
     DISPLAY_SELECT <= '1'; 
-    wait for 10 ps;
-    waitForClockToFall;
+    --wait for 10 ps;
 
+    waitForClockToFall;
+    COOL <= '1';
+    -- STATE: COOL_ON
+
+    waitForClockToFall;
+    AC_READY <= '1';
+    -- STATE: AC_NOW_READY
+
+    waitForClockToFall;
+    DESIRED_TEMP <= "1111111";
+    CURRENT_TEMP <= "0000000";
+    DISPLAY_SELECT <= '0';
+    AC_READY <= '0';
+    --wait for 10 ps;
+
+    waitForClockToFall;
     HEAT <= '1';
-    wait for 10 ps;
+    
     waitForClockToFall;
+    FURNACE_HOT <= '1';
 
+
+    waitForClockToFall;
     CURRENT_TEMP <= "0000000";
     DESIRED_TEMP <= "1111111";
 
-    wait for 10 ps;
+    --wait for 10 ps;
     waitForClockToFall;
-
     HEAT <= '0';
 
-    wait for 10 ps;
+    --wait for 10 ps;
     waitForClockToFall;
+    FURNACE_HOT <= '0';
 
-    COOL <= '1';
-
-    wait for 10 ps;
+    --wait for 10 ps;
     waitForClockToFall;
-    
     CURRENT_TEMP <= "1111111";
     DESIRED_TEMP <= "0000000";
-    wait for 10 ps;
-    waitForClockToFall;
+    --wait for 10 ps;
 
+    waitForClockToFall;
     COOL <= '0';
-    wait for 10 ps;
+    --wait for 10 ps;
     waitForClockToFall;
-
     wait;
 end process;
 
-
-
-
-
 end TEST;
-
-    
-
--- work/vhdl_thermostat.vhd
+++ work/vhdl_thermostat.vhd
@@ -4,73 +4,166 @@ use ieee.std_logic_1164.all;
 entity THERMOSTAT is
     port (
 
-        CLK: in bit; 
+        CLK: in std_logic; 
 
         CURRENT_TEMP: in std_logic_vector(6 downto 0);
         DESIRED_TEMP: in std_logic_vector(6 downto 0);
-        DISPLAY_SELECT: in bit;
-        COOL: in bit;   
-        HEAT: in bit; 
+        DISPLAY_SELECT: in std_logic;
+        COOL: in std_logic;
+        HEAT: in std_logic; 
         TEMP_DISPLAY: out std_logic_vector(6 downto 0);
-        AC_ON: out bit; 
-        FURNACE_ON: out bit
+
+        AC_ON: out std_logic; 
+        AC_READY: in std_logic; 
+
+        FURNACE_ON: out std_logic;
+        FURNACE_HOT: in std_logic;
+
+        FAN_ON: out std_logic
     );
 end THERMOSTAT;
 
 
 architecture BEHAVIOUR of THERMOSTAT is
 
+    type THERMOSTAT_STATE is (IDLE,
+                            COOL_ON,
+                            AC_NOW_READY,
+                            AC_DONE,
+                            HEAT_ON,
+                            FURNACE_NOW_HOT,
+                            FURNACE_COOL);
+
+
     signal CURRENT_TEMP_REG, DESIRED_TEMP_REG: std_logic_vector(6 downto 0);
-    signal COOL_REG, HEAT_REG, DISPLAY_SELECT_REG: bit;
+    
+    signal COOL_REG: std_logic; 
+    signal HEAT_REG: std_logic;  
+    signal DISPLAY_SELECT_REG: std_logic;  
+    signal FURNACE_HOT_REG: std_logic;  
+    signal AC_READY_REG: std_logic;
+
+    signal CURRENT_STATE, NEXT_STATE: THERMOSTAT_STATE; 
 
 begin
 
+    -- INPUT INGESTION
     process(CLK)
     begin
-
         if CLK'event and CLK = '1' then
             CURRENT_TEMP_REG <= CURRENT_TEMP;
             DESIRED_TEMP_REG <= DESIRED_TEMP;
             COOL_REG <= COOL;
             HEAT_REG <= HEAT;
             DISPLAY_SELECT_REG <= DISPLAY_SELECT;
+            FURNACE_HOT_REG <= FURNACE_HOT;
+            AC_READY_REG <= AC_READY;
 
             if DISPLAY_SELECT_REG = '1' then
                 TEMP_DISPLAY <= CURRENT_TEMP_REG;
             else
                 TEMP_DISPLAY <= DESIRED_TEMP_REG;
             end if;
-
-
         end if; 
     end process;
 
+
+    -- STATE MACHINE OUTPUT
+    -- I GUESS IT'S A MATTER OF STYLE TO MAKE THE OUTPUT SIGNAL OR THE STATE THE HEADER OF EACH BLOCK
+    -- I FIND THIS TO BE PLENTY READABLE BUT REALLY STRESSFUL TO WRITE
     process(CLK)
     begin 
+        CURRENT_STATE <= NEXT_STATE;
         if CLK'event and CLK = '1' then
-            if DESIRED_TEMP_REG < CURRENT_TEMP_REG then
-                if COOL_REG = '1' then
-                    AC_ON <= '1';
-                else
+            case NEXT_STATE is  
+                when IDLE =>
+                    FURNACE_ON <= '0';
                     AC_ON <= '0';
-                end if; 
-            
-            else
-                if DESIRED_TEMP_REG > CURRENT_TEMP_REG then
-                    if HEAT_REG = '1' then
-                        FURNACE_ON <= '1';
-                    else
-                        FURNACE_ON <= '0';
-                    end if;
+                    FAN_ON <= '0';
                 
-                else 
-                    if DESIRED_TEMP = CURRENT_TEMP then
-                        FURNACE_ON <= '0';
-                        AC_ON <= '0';
-                    end if;
-                end if;
-            end if; 
+                when COOL_ON =>
+                    FURNACE_ON <= '0';
+                    AC_ON <= '1';
+                    FAN_ON <= '0';
+                
+                when AC_NOW_READY => 
+                    FURNACE_ON <= '0';
+                    AC_ON <= '1';
+                    FAN_ON <= '1';
+                
+                when AC_DONE =>
+                    FURNACE_ON <= '0';
+                    AC_ON <= '0';
+                    FAN_ON <= '1';
+                
+                when HEAT_ON =>
+                    FURNACE_ON <= '1';
+                    AC_ON <= '0';
+                    FAN_ON <= '0';
+                
+                when FURNACE_NOW_HOT =>
+                    FURNACE_ON <= '1';
+                    AC_ON <= '0';
+                    FAN_ON <= '1';
+                
+                when FURNACE_COOL =>
+                    FURNACE_ON <= '0';
+                    AC_ON <= '0';
+                    FAN_ON <= '1';    
+            end case; 
         end if; 
+    end process; 
+    
+
+    -- NEXT STATE COMPUTATION
+    process (CURRENT_STATE, DESIRED_TEMP_REG, CURRENT_TEMP_REG, HEAT_REG, COOL_REG, AC_READY_REG, FURNACE_HOT_REG)
+    begin
+        NEXT_STATE <= CURRENT_STATE;
+        case CURRENT_STATE is  
+            when IDLE =>
+            if COOL_REG = '1' and CURRENT_TEMP_REG > DESIRED_TEMP_REG then
+                NEXT_STATE <= COOL_ON; 
+            elsif HEAT_REG = '1' and CURRENT_TEMP_REG < DESIRED_TEMP_REG then
+                NEXT_STATE <= HEAT_ON;
+            end if; 
+
+            when COOL_ON => 
+            if AC_READY_REG = '1' then
+                NEXT_STATE <= AC_NOW_READY;
+            end if; 
+            
+            when AC_NOW_READY => 
+            if COOL_REG = '0' or CURRENT_TEMP_REG <= DESIRED_TEMP_REG then
+                NEXT_STATE <= AC_DONE;
+            end if; 
+
+            when AC_DONE => 
+            if AC_READY_REG = '0' then
+                NEXT_STATE <= IDLE;
+            end if; 
+            
+            when HEAT_ON => 
+            if FURNACE_HOT_REG = '1' then
+                NEXT_STATE <= FURNACE_NOW_HOT;
+            end if; 
+
+            when FURNACE_NOW_HOT => 
+            if HEAT_REG = '0' or CURRENT_TEMP_REG >= DESIRED_TEMP_REG then
+                NEXT_STATE <= FURNACE_COOL;
+            elsif FURNACE_HOT_REG = '0' then
+                NEXT_STATE <= IDLE; 
+            end if;
+
+            when FURNACE_COOL =>
+            if FURNACE_HOT_REG = '0' then
+                NEXT_STATE <= IDLE;
+            end if;
+            
+            when others =>
+                NEXT_STATE <= IDLE;
+        end case; 
+        -- WITH VHDL, IT IS WAY MORE IMPORTANT TO PRIORITIZE DESCRIPTIVENES OVER (SOFTWARE) EFFICIENCY
+        -- NESTED IFS CAN BE DIFFICULT TO READ. ME SEF, I FEEL AM AS I DEY WRITE AM
         -- LMAO. THIS IS SO NOOBISH. DOG DAYS FR
     end process;
 
